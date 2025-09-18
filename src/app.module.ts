89ea95cb2175ea379992a59a0abc517e0813f932
Begmyrat <begmyratgm@gmail.com>
2025-09-18 17:01:30 +0500

    setup modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { PhotosModule } from './photos/user-photo.module';
+import { InterestsModule } from './interests/interests.module';
+import { MatchesModule } from './matches/matches.module';
+import { ChatsModule } from './chats/chats.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { AgreementsModule } from './agreements/agreements.module';
+import { UploadsModule } from './uploads/uploads.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ProfilesModule,
+    PhotosModule,
+    InterestsModule,
+    MatchesModule,
+    ChatsModule,
+    NotificationsModule,
+    AgreementsModule,
+    UploadsModule,
+  ],
   controllers: [],
   providers: [],
 })
